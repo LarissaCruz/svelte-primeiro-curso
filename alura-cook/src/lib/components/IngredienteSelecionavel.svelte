d4a500ad0ce358f285d4c639550846e6cde7185a
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2023-05-09 14:15:33 -0300

    Landing page implementation

---
+++ alura-cook/src/lib/components/IngredienteSelecionavel.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import Tag from "./Tag.svelte";
+  export let ingrediente: string;
+
+  const dispatch = createEventDispatcher<{
+    adicionarIngrediente: string;
+    removerIngrediente: string;
+  }>();
+
+  let selecionado: boolean = false;
+
+  function aoClicar() {
+    selecionado = !selecionado;
+    if (selecionado) {
+      dispatch("adicionarIngrediente", ingrediente);
+    } else {
+      dispatch("removerIngrediente", ingrediente);
+    }
+  }
+</script>
+
+<button on:click={aoClicar}>
+  <Tag ativa={selecionado}>{ingrediente}</Tag>
+</button>
+
+<style>
+  button {
+    cursor: pointer;
+  }
+</style>
---
+++ alura-cook/src/routes/receitas/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Alura Cook | Receitas</title>
+</svelte:head>
+
+<h1>Welcome to Receitas</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<a href="/">Voltar</a>
